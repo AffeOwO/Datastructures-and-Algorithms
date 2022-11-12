a510b4dfe09705fea852a37bc6f46315b4735f18
Affe <affeowo@outlook.de>
2022-11-12 23:42:03 +0100

    Stack.py

--- Datastructures/Stack.py
+++ Datastructures/Stack.py
@@ -1,12 +1,21 @@
 class Stack:
-    def __init__(self, head=None):
-        self.head = [head]
+    def __init__(self):
+        self.head = []
 
     def push(self, new_element):
         self.head.append(new_element)
 
     def pop(self):
+        if not self.head:
+            raise "IndexError"
         self.head.pop()
 
     def peek(self):
+        if not self.head:
+            raise "IndexError"
         return self.head[-1]
+
+
+if __name__ == "__main__":
+    test = Stack()
+    test.peek()
