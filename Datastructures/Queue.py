2fcb1c2a158f6d6b122438f86cc0165f23cb4c2c
Affe <affeowo@outlook.de>
2022-11-12 23:41:49 +0100

    Queue.py

--- Datastructures/Queue.py
+++ Datastructures/Queue.py
@@ -1,12 +1,21 @@
 class Queue:
-    def __init__(self, head=None):
-        self.head = [head]
+    def __init__(self):
+        self.head = []
 
     def push(self, new_element):
         self.head.append(new_element)
 
     def pop(self):
+        if not self.head:
+            raise "IndexError"
         self.head.pop(0)
 
     def peek(self):
+        if not self.head:
+            raise "IndexError"
         return self.head[0]
+
+
+if __name__ == "__main__":
+    test = Queue()
+    test.peek()
