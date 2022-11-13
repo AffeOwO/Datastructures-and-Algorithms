9e08e0e97d97d2a77372ae10c63c5229cd0b1bcf
Affe <affeowo@outlook.de>
2022-11-13 14:53:19 +0100

    Linear Search.py

--- Algorithms/Search/Linear Search.py
+++ Algorithms/Search/Linear Search.py
@@ -1,5 +1,8 @@
-def linear_search(array, value):
+def linear_search(array: list, value) -> int:
     for index, i in enumerate(array):
         if i == value:
             return index
     return -1
+
+if __name__ == "__main__":
+    pass
