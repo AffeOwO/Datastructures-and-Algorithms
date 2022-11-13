5b6576a31855296493abbaec75b874675091f22f
Affe <affeowo@outlook.de>
2022-11-13 06:54:09 +0100

    Binary Search.py

---
+++ Algorithms/Binary Search.py
@@ -0,0 +1,19 @@
+def binary_search(array, value):
+    start = 0
+    end = len(array) - 1
+
+    while start <= end:
+        mid = int(start + (end - start) / 2)
+        if value == array[mid]:
+            return mid
+        elif value < array[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    # if not in array
+    return -1
+
+
+if __name__ == "__main__":
+    print(binary_search([1, 3, 9, 11, 15, 19, 29], 15))
