85c40e936eab59d778da0558aaf59f6d24a95349
Affe <affeowo@outlook.de>
2022-11-14 20:41:27 +0100

    Bubble Sort.py

---
+++ Algorithms/Sort/Bubble Sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(array: list):
+    length = len(array) - 1
+    for _ in range(length):
+        for i in range(length):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+    return array
+
+
+if __name__ == "__main__":
+    print(bubble_sort([9-i for i in range(10)]))
+
+    # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
