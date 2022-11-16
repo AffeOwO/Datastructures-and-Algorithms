e44085a025b02c99ca3bc005bd5af7efa96459d1
Affe <affeowo@outlook.de>
2022-11-16 21:48:17 +0100

    Get Square Root.py

---
+++ Practice/Get Square Root.py
@@ -0,0 +1,25 @@
+# input >= 0
+
+def solution_1(x):
+    return x ** 0.5
+
+
+def solution_2(x):
+    res = 0
+    while res ** 2 <= x:
+        res += 1
+    return (res - 1) // 1
+
+
+# newton
+def solution_3(x):
+    r = x
+    while r ** 2 > x:
+        r = (r + x/r) / 2
+    return r
+
+
+if __name__ == "__main__":
+    print(solution_1(484))
+    print(solution_2(484))
+    print(solution_3(484))
