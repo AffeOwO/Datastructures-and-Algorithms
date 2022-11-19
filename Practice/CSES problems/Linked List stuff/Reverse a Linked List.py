2534f6adb3752d4f245c1e1ed38c2ebfebe18169
Affe <affeowo@outlook.de>
2022-11-19 15:48:29 +0100

    Linked List stuff

---
+++ Practice/CSES problems/Linked List stuff/Reverse a Linked List.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sol = rev_head = ListNode()
+        temp = []
+        while head:
+            temp.append(head.val)
+            head = head.next
+        for i in temp[::-1]:
+            sol.next = ListNode(i)
+            sol = sol.next
+        return rev_head.next
---
+++ Practice/CSES problems/Permutations.py
@@ -0,0 +1,5 @@
+num = int(input())
+
+if num in (2, 3):
+    print("NO SOLUTION")
+    exit()
