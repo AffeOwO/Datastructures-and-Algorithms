af2f158d947b3ba6e2db891cbd5cc85df6361f78
Affe <affeowo@outlook.de>
2022-11-22 21:44:46 +0100

    Number Spiral.py

--- Practice/CSES problems/Number Spiral.py
+++ Practice/CSES problems/Number Spiral.py
@@ -1,5 +1,15 @@
 test_cases = int(input())
 
 for i in range(test_cases):
-    y, x = map(int, input().split())
-#ToDo: task
+    y_coord, x_coord = map(int, input().split())
+    if y_coord < x_coord:
+        if x_coord % 2 == 1:
+            print(x_coord * x_coord - y_coord + 1)
+        else:
+            print((x_coord - 1) ** 2 + y_coord)
+    else:
+        if y_coord % 2 == 0:
+            print(y_coord * y_coord - x_coord + 1)
+        else:
+            print((y_coord - 1) ** 2 + x_coord)
+
