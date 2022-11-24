2afc74b6d915ca8a833944b7bfabc1786f87809b
Affe <affeowo@outlook.de>
2022-11-24 22:32:16 +0100

    cses

---
+++ Practice/CSES problems/Dynamic Programming/Dice Combinations.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+if n <= 6:
+    print(2**(n-1))
+
+# ToDo: n>6 solution implementation
---
+++ Practice/CSES problems/Sorting and Searching/Distinct Numbers.py
@@ -0,0 +1,4 @@
+values_len = int(input())
+values = list(map(int, input().split()))
+
+print(len(set(values)))
