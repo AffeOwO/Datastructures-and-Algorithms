e07f4c1c7523776bfd5b86311274f78e749682e7
Affe <affeowo@outlook.de>
2022-11-24 22:07:25 +0100

    Fibonacci.py normal implemented(non recursive)

--- Practice/Random/Fibonacci.py
+++ Practice/Random/Fibonacci.py
@@ -5,14 +5,25 @@ from functools import cache
 
 
 @cache
-def get_fib(n: int):
+def get_fib_recursive(n: int):
     if n < 0:
         raise ValueError("must be >= 0")
     if n in (0, 1):
         return n
-    return get_fib(n - 1) + get_fib(n - 2)
+    return get_fib_recursive(n - 1) + get_fib_recursive(n - 2)
+
+
+def get_fib(n: int):
+    a = b = 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
 
 if __name__ == "__main__":
     for i in range(1, 10001):
         print(f"{i}: {get_fib(i)}")
+    for i in range(1, 10001):
+        print(f"{i}: {get_fib_recursive(i)}")
+
+    # get_fib_recursive() is faster bcs of caching
