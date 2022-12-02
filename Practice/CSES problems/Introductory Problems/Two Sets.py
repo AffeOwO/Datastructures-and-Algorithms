999acf12e26c890922c874c2a5343a115579107f
Affe <affeowo@outlook.de>
2022-12-02 22:57:18 +0100

    cses

--- Practice/CSES problems/Introductory Problems/Two Sets.py
+++ Practice/CSES problems/Introductory Problems/Two Sets.py
@@ -1,3 +1,21 @@
 n = int(input())
 
-# ToDo: solve Task
+temp = 0
+for i in range(1, n+1):
+    temp += i
+
+if temp % 2 != 0:
+    print("No")
+    exit()
+
+
+set1 = set()
+set2 = set()
+
+# ToDo: get the content of both sets
+
+print("Yes")
+print(len(set1))
+print(set1)
+print(len(set2))
+print(set2)
