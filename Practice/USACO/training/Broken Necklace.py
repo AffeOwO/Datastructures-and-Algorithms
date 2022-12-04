0ed08d4f508efcf2c64ead064b471e7504fc75e7
Affe <affeowo@outlook.de>
2022-12-05 00:36:15 +0100

    usaco

---
+++ Practice/USACO/training/Broken Necklace.py
@@ -0,0 +1,18 @@
+"""
+ID: yamato berner (affeowo1)
+TASK: beads
+LANG: PYTHON3
+"""
+
+fin = open("temp.in", "r")
+fout = open("temp.out", "w")
+
+fin = fin.readlines()
+
+beads = int(fin[0])
+necklace = fin[1].strip()
+temp = []
+
+temp2 = 0
+for i in range(beads - 1):
+    pass
