a059f9138b3075149cf598d1a2e7f798b90b8216
Affe <affeowo@outlook.de>
2022-12-06 19:07:10 +0100

    ToDo

---
+++ Practice/USACO/training/Section 1/Milking Cows.py
@@ -0,0 +1,17 @@
+"""
+ID: yamato berner (affeowo1)
+TASK: milk2
+LANG: PYTHON3
+"""
+
+fin = open("temp.in", "r")
+fout = open("temp.out", "w")
+
+fin = fin.readlines()
+longest, longest_non = 0, 0
+intervals = []
+
+# ToDo: solve question
+
+fout.write(f"{longest} {longest_non}\n")
+fout.close()
