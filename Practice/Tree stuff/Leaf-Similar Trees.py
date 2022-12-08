0404d677e883886939f07c94157aa1059cd50319
Affe <affeowo@outlook.de>
2022-12-09 00:35:08 +0100

    tree

---
+++ Practice/Tree stuff/Leaf-Similar Trees.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import itertools
+
+class Solution:
+    def leafSimilar(self, root1, root2):
+        def dfs(node):
+            if not node: return
+            if not node.left and not node.right: yield node.val
+            for i in dfs(node.left): yield i
+            for i in dfs(node.right): yield i
+        return all(a == b for a, b in itertools.zip_longest(dfs(root1), dfs(root2)))
