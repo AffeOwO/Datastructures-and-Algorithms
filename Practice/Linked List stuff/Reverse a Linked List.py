113855c4a9730948c6e524cfeaedcf937f0b4ff8
Affe <affeowo@outlook.de>
2022-12-18 00:05:06 +0100

    LinkedList

--- Practice/Linked List stuff/Reverse a Linked List.py
+++ Practice/Linked List stuff/Reverse a Linked List.py
@@ -14,3 +14,18 @@ class Solution:
             sol.next = ListNode(i)
             sol = sol.next
         return rev_head.next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sol = ListNode(head.val)
+        head = head.next
+        while head:
+            temp = ListNode(head.val, sol)
+            sol = temp
+            head = head.next
+        return sol
