eb98856dad7c3b9e56cf075e714d886895f0d13b
Affe <affeowo@outlook.de>
2022-12-18 01:42:15 +0100

    Array and String stuff

--- Practice/Array and String stuff/Largest Number At Least Twice of Others.py
+++ Practice/Array and String stuff/Largest Number At Least Twice of Others.py
@@ -1,4 +1,4 @@
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
         temp = sorted(set(nums))
-        return nums.index(temp[-1]) if temp[-1]*2 >= temp[-2] else -1
+        return nums.index(temp[-1]) if temp[-1] >= temp[-2]*2 else -1
