09d8fd4fc1b6fe16be4d8905561a4747ca2073dc
Affe <affeowo@outlook.de>
2022-12-21 20:07:07 +0100

    random

--- Practice/Random/Check if n is power of 4.py
+++ Practice/Random/Check if n is power of 4.py
@@ -1,3 +1,3 @@
 class Solution:
     def isPowerOfFour(self, num):
-        return num > 0 and num & (num-1) == 0 and 0b1010101010101010101010101010101 & num == num
+        return 0b1010101010101010101010101010101 & num == num > 0 == num & (num - 1)
