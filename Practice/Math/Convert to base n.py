033c5663a956f8704e70fc6926ae104eb953656b
Affe <affeowo@outlook.de>
2022-12-29 00:46:17 +0100

    math

--- Practice/Math/Convert to base n.py
+++ Practice/Math/Convert to base n.py
@@ -1,5 +1,6 @@
+# base < 10
 class Solution:
-    def convertToBase7(self, n: int, base: int):
-        if n < 0: return '-' + self.convertToBase7(-n)
+    def convertToBase(self, n: int, base: int):
+        if n < 0: return '-' + self.convertToBase(-n)
         if n < base: return str(n)
-        return self.convertToBase7(n // base) + str(n % base)
+        return self.convertToBase(n // base) + str(n % base)
