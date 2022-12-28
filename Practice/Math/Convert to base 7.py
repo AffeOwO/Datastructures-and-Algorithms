f370306af7fc81c7349edc8e516ab8950d11f45f
Affe <affeowo@outlook.de>
2022-12-28 23:58:59 +0100

    math

---
+++ Practice/Math/Convert to base 7.py
@@ -0,0 +1,5 @@
+class Solution:
+    def convertToBase7(self, n):
+        if n < 0: return '-' + self.convertToBase7(-n)
+        if n < 7: return str(n)
+        return self.convertToBase7(n // 7) + str(n % 7)
