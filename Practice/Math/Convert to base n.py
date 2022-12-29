eca9a93da1a82b04114140f9b5566a72fac8a804
Affe <affeowo@outlook.de>
2022-12-29 01:04:20 +0100

    math

--- Practice/Math/Convert to base n.py
+++ Practice/Math/Convert to base n.py
@@ -1,6 +1,11 @@
-# base < 10
-class Solution:
-    def convertToBase(self, n: int, base: int):
-        if n < 0: return '-' + self.convertToBase(-n)
-        if n < base: return str(n)
-        return self.convertToBase(n // base) + str(n % base)
+digits = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J']  # (first 20 digits)
+def to_base(num):
+    if base == 10:
+        return num
+    if num < base: return digits[num]
+    return to_base(num // base) + digits[num % base]
+
+
+if __name__ == "__main__":
+    base = int(input())
+    print(to_base(int(input())))
