df19c285a6ddbd3d6b39789380585cd7da02295b
Affe <affeowo@outlook.de>
2022-12-31 20:32:47 +0100

    cses

--- Practice/CSES problems/Introductory Problems/Two Sets.py
+++ Practice/CSES problems/Introductory Problems/Two Sets.py
@@ -1,21 +1,19 @@
 n = int(input())
 
-temp = 0
-for i in range(1, n+1):
-    temp += i
-
-if temp % 2 != 0:
-    print("No")
+if n % 4 in (1, 2):
+    print("NO")
     exit()
 
+# set1 and set2 aren't actually sets, that's just how they were called in the question
+if n % 2 == 0:
+    set1 = [i for i in range(1, n+1) if i <= n // 4 or i > n // 4 * 3]
+    set2 = [i for i in range(1, n+1) if n // 4 < i <= n // 4 * 3]
+else:
+    set1 = [i for i in range(1, n+1) if i <= n // 4 or i >= n - n // 4]
+    set2 = [i for i in range(1, n+1) if n // 4 < i < n - n // 4]
 
-set1 = set()
-set2 = set()
-
-# ToDo: get the content of both sets
-
-print("Yes")
+print("YES")
 print(len(set1))
-print(set1)
+print(*set1)
 print(len(set2))
-print(set2)
+print(*set2)
