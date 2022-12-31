661b603e72a8bb58f8ea05fb719465df5e987871
Affe <affeowo@outlook.de>
2022-12-31 22:05:43 +0100

    cses

--- Practice/CSES problems/Introductory Problems/Two Sets.py
+++ Practice/CSES problems/Introductory Problems/Two Sets.py
@@ -5,12 +5,16 @@ if n % 4 in (1, 2):
     exit()
 
 # set1 and set2 aren't actually sets, that's just how they were called in the question
+set1 = []
+set2 = []
 if n % 2 == 0:
-    set1 = [i for i in range(1, n+1) if i <= n // 4 or i > n // 4 * 3]
-    set2 = [i for i in range(1, n+1) if n // 4 < i <= n // 4 * 3]
+    for i in range(1, n+1):
+        if i <= n // 4 or i > n // 4 * 3: set1.append(i)
+        else: set2.append(i)
 else:
-    set1 = [i for i in range(1, n+1) if i <= n // 4 or i >= n - n // 4]
-    set2 = [i for i in range(1, n+1) if n // 4 < i < n - n // 4]
+    for i in range(1, n+1):
+        if i <= n // 4 or i >= n - n // 4: set1.append(i)
+        else: set2.append(i)
 
 print("YES")
 print(len(set1))
