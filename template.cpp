8a1345b023436c240550478ada83598d046df9d8
Affe <affeowo@outlook.de>
2023-02-21 16:08:25 +0100

    template

--- template.cpp
+++ template.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
 #define endl "\n"
 
 int main() {
+    //ifstream fin("")
+    //ofstream fout("")
+
     ios::sync_with_stdio(0);
     cin.tie(0);
     
