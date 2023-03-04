a3db82cff17ef261ebfc3884fde82222319db856
Affe <affeowo@outlook.de>
2023-03-04 22:11:28 +0100

    cses

---
+++ Practice/CSES problems/Sorting and Searching/Distinct Numbers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <string>
+//#include <math.h>
+//#include <algorithm>
+//#include <vector>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    unordered_set<int> hashy;
+    int n; cin >> n;
+    for(int i=0; i<n; i++) {
+        int temp; cin >> temp;
+        hashy.insert(temp);
+    }
+    cout << size(hashy) << endl;
+}
--- Practice/CSES problems/Sorting and Searching/Distinct Numbers.py
+++
@@ -1,4 +0,0 @@
-values_len = int(input())
-values = list(map(int, input().split()))
-
-print(len(set(values)))
