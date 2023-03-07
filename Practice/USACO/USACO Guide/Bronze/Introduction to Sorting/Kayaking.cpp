b6bdf5461282613dce218bbcb97a1abe5445edb2
Affe <affeowo@outlook.de>
2023-03-07 18:16:45 +0100

    cf

--- Practice/USACO/USACO Guide/Bronze/Introduction to Sorting/Kayaking.cpp
+++ Practice/USACO/USACO Guide/Bronze/Introduction to Sorting/Kayaking.cpp
@@ -24,5 +24,28 @@ int main() {
         cin >> nums[i];
     }
     sort(nums, nums+2*n);
-    
+    int sol = 100000000;
+    // prolly O(n^3)
+    for(int i=0; i<n*2; i++) {
+        for(int j=i+1; j<n*2; j++) {
+            int cur = 0;
+            //bruh im terrible at this :skull:
+            int temp_arr[2];
+            int temp_pnt = 0;
+            for(int o=0; o<n*2; o++) {
+                if(o==i || o==j) continue;
+                temp_arr[temp_pnt] = nums[o];
+                temp_pnt++;
+                if(temp_pnt==2) {
+                    temp_pnt = 0;
+                    int sum_temp = temp_arr[1] - temp_arr[0];
+                    cur += sum_temp;
+                }
+            }
+
+            if(cur<sol) sol = cur;
+        }
+    }
+
+    cout << sol << endl;
 }
