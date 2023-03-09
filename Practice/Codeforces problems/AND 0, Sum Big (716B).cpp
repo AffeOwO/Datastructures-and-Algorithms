47751323c685f63220c51d04f3b65b606a4407fb
Affe <affeowo@outlook.de>
2023-03-09 20:42:39 +0100

    codeforces

---
+++ Practice/Codeforces problems/AND 0, Sum Big (716B).cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <string>
+//#include <math.h>
+//#include <algorithm>
+//#include <vector>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+
+void solve() {
+    ll n, k; cin >> n >> k;
+    ll sol = 1;
+    ll mod = pow(10, 9) + 7;
+    for(int i=0; i<k; i++) sol = sol*n % mod;
+    cout << sol << endl;
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) solve();
+}
