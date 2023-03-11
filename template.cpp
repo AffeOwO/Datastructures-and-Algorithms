0bd4841e83ea66f432dfcd539a7d9fe72e40f624
Affe <affeowo@outlook.de>
2023-03-11 01:24:45 +0100

    template

--- template.cpp
+++ template.cpp
@@ -10,7 +10,8 @@ using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 #define endl "\n"
-#define all(x) (x).begin(), (x).end()
+#define all(x) (x).begin(), (x).end();
+#define MOD 1000000007;
 
 int main() {
     //ifstream fin("");
