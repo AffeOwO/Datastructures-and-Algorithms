4dcb65366365a8423a2f788c27e349ef5bbcb788
Affe <affeowo@outlook.de>
2023-03-14 18:20:48 +0100

    codeforces

---
+++ Practice/Codeforces problems/Coloring Rectangles (1584B).cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <string>
+//#include <math.h>
+//#include <algorithm>
+//#include <vector>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+
+void solve() {
+    int n, m; cin >> n >> m;
+    // try to get as many 3x1 pieces since they only require one blue
+    int sol = (n*m)/3;
+    if((n*m)%3!=0) sol++;
+    cout << sol << endl;
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) solve();
+}
