fa0dbf09a4c46da09a4f59b0095c1b267be8763f
Affe <affeowo@outlook.de>
2023-04-07 04:13:56 +0200

    idki

---
+++ Practice/Codeforces problems/Polycarp and Coins (1551A).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <string>
+//#include <math.h>
+//#include <algorithm>
+//#include <vector>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+
+void solve() {
+    int n; cin >> n;
+    pair<int, int> sol;
+    sol.first = n/3;
+    sol.second = n%3;
+    if(sol.second == 2) cout << sol.first << " " << sol.first + 1<< endl;
+    else cout << sol.first+sol.second << " " << sol.first << endl;
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) solve();
+}
