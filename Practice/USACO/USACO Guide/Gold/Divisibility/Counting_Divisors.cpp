e0353d8f12ea10ea5e6a9cbebb8995fdff1cad71
Affe <affeowo@outlook.de>
2023-04-27 00:39:54 +0200

    usaco guide

---
+++ Practice/USACO/USACO Guide/Gold/Divisibility/Counting_Divisors.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <string>
+//#include <math.h>
+//#include <algorithm>
+//#include <vector>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+
+void solve() {
+    int n; cin >> n;
+    int sol = 0;
+    for(int i=1; i<=sqrt(n); i++) {
+        if(n%i==0) {
+            if(i*i==n) sol++;
+            else sol += 2;
+        }
+    }
+    cout << sol << endl;
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) solve();
+}
