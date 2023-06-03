5b2df3970fd1f482d00664f61557bb125d60fba3
Affe <affeowo@outlook.de>
2023-06-03 18:57:56 +0200

    cf

---
+++ Practice/Codeforces problems/Jumps (1455B).cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <string>
+//#include <math.h>
+//#include <algorithm>
+//#include <vector>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+
+void solve() {
+    int x; cin >> x;
+    int sol = 0;
+    while(sol*(sol+1)<2*x)  sol++;
+    if(sol*(sol+1)/2==x+1) sol++;
+    cout << sol << endl;
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) solve();
+}
