d79c9513a6c62151d3453ac62b83be1d497df4ee
Affe <affeowo@outlook.de>
2023-06-03 16:22:30 +0200

    cf

---
+++ Practice/Codeforces problems/Dislike of Threes (1560A).cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <string>
+//#include <math.h>
+//#include <algorithm>
+//#include <vector>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+
+void solve() {
+    int k; cin >> k;
+    int cur = 0;
+    while(k!=0) {
+        cur++;
+        if(cur%3!=0 and cur%10!=3) k--;
+    }
+    cout << cur << endl;
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) solve();
+}
