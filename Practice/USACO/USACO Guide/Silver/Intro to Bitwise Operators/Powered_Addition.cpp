354b30dac23851b4b2a3cb6efc3fbdcaec3e0c42
Affe <affeowo@outlook.de>
2023-06-07 23:19:11 +0200

    cf

--- Practice/USACO/USACO Guide/Silver/Intro to Bitwise Operators/Powered_Addition.cpp
+++ Practice/USACO/USACO Guide/Silver/Intro to Bitwise Operators/Powered_Addition.cpp
@@ -10,10 +10,21 @@ using ld = long double;
 
 void solve() {
     int n; cin >> n;
-    vector<int> nums(n); // 1-indexed
-    for(auto &x: nums) cin >> x;
-
-    
+    vector<ll> nums(n); // 1-indexed
+    ll sol = 0;
+    ll maxi = -1e9-1, max_dif = 0;
+    for(int i=0; i<n; i++) {
+        int temp; cin >> temp;
+        nums[i] = temp;
+        if(temp>maxi) maxi = temp;
+        max_dif = max(max_dif, maxi-temp); 
+    }
+    ll cur = 1;
+    while(cur-1<max_dif) {
+        sol++;
+        cur*=2;
+    }
+    cout << sol << endl;
 }
 
 int main() {
