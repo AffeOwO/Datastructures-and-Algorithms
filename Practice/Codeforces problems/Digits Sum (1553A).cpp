453f67682658a4ed237d636aef9d430e2778ccd0
Affe <affeowo@outlook.de>
2023-07-07 22:59:09 +0200

    cf

---
+++ Practice/Codeforces problems/Digits Sum (1553A).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+
+void solve() {
+    int n; cin >> n; // find all x, such that 1 <= x <= n
+    // x is interesting if S(x+1)<S(x) where S(x) is the sum of digits of x
+    cout << (n+1)/10 << endl;
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) solve();
+}
