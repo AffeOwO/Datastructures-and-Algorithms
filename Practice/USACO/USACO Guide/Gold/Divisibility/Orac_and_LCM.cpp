b14d91c1c9779856db5daf023451c609ab04207c
Affe <affeowo@outlook.de>
2023-07-16 11:02:08 +0200

    cf

--- Practice/USACO/USACO Guide/Gold/Divisibility/Orac_and_LCM.cpp
+++ Practice/USACO/USACO Guide/Gold/Divisibility/Orac_and_LCM.cpp
@@ -8,50 +8,73 @@ using ld = long double;
 #define all(x) (x).begin(), (x).end()
 #define MOD 1000000007
 
+vector<int> primes(2e5+1);
+void compute() {
+    primes[1] = 1;
+    for(int i=2; i<=2e5; i++) {
+        if(!primes[i]) {
+            for(int j=i; j<=2e5; j+=i) primes[j] = i;
+        }
+    }
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    compute();
+    // for(int i=1; i<=100; i++) cout << primes[i] << " ";
+
     int n; cin >> n;
-    vector<int> s(n);
-    for(auto &x: s) cin >> x;
-    // prime factorization
-    // there are n(n-1)/2 pairs -> bruteforce too slow
-    // calculate the gcd({lcm(a, b) | a, b in s})
-    // -> gcd({lcm(a, b) | a, b in s}) >= gcd(s)
-    // every prime factor which is in at least n-1 numbers is also in the gcd since the lcm will include it as long as there is no pair of numbers not containing it
-    
-    /* time complexities:
-    maybe precompute prime numbers O(sqrt(max(m))*log(log(sqrt(max(m)))))
-    prime factorization of n numbers -> O(n*sqrt(m_i)) where m_i is the ith number
-    maybe make the final map more efficient
-    check for final map O(n*p) 
+    vector<int> nums(n);
+    for(auto &x: nums) cin >> x;
+
+    // find gcd{lcm{nums[i], nums[j]} | 1 <= i < j <= n}
+    /*
+    lcm{a; b; c} = lcm{a; lcm{b; c}}
+    every prime factor which appears in at least n-1 numbers will be in the gcd
     */
+   	ll sol = 1;
 
-    // todo do later
-    
-}
-
-/*
-map<int, pair<int, int>> final; // .first > .second
-    for(auto &x: s) {
-        for(int i=2; i<=sqrt(200000)+1; i++) {
-            int cnt = 0;
-            while(x%i==0) {
-                cnt++;
-                x/=i;
+    map<int, pair<int, int>> factors;
+    map<int, int> cnt;
+    for(int i=0; i<n; i++) {
+        pair<int, int> cur = {primes[nums[i]], 0}; //.first = num; .second = cnt
+        while(nums[i]>1) {
+            int temp = primes[nums[i]];
+            if(temp != cur.first or nums[i] == temp) {
+                //if(cur.first == 2) cout << i << " " << cur.second << endl;
+                cnt[cur.first]++;
+                //if(cur.first == 2) cout << i << " " << cur.second << endl;
+                if(nums[i] == temp and temp != cur.first) {
+                    // cnt = 1
+                    cnt[temp]++;
+                    if(!factors.count(temp)) factors[temp] = {690, 690};
+                    //cout << factors[temp].first << " " << factors[temp].second << " " << temp << endl;
+                    if(factors[temp].first == 1) factors[temp].second = 1;
+                    factors[temp] = {1, factors[temp].first};
+                    //cout << factors[temp].first << " " << factors[temp].second << " " << temp << endl;
+                }
+                else if(nums[i] == temp) cur.second++;
+                //if(i==0) cout << nums[i] << " " << temp << " " << cur.first << " " << cur.second << endl;
+                if(!factors.count(cur.first)) {
+                    factors[cur.first] = {690, 690};
+                }
+                if(cur.second<factors[cur.first].second) factors[cur.first].second = cur.second;
+                if(factors[cur.first].second<factors[cur.first].first) factors[cur.first] = {factors[cur.first].second, factors[cur.first].first};
+                cur = {temp, 0};
             }
-            if(!final.count(i)) final[i] = pair<int, int>{20, cnt};
-            else {
-                if(cnt<final[i].second) final[i] = pair<int, int>{final[i].second, cnt};
-                else if(cnt<final[i].first and cnt>final[i].second) final[i].first = cnt;
-                else if(cnt==final[i].second) final[i].first = final[i].second;
-            }
+            cur.second++;
+            nums[i] /= temp;
         }
     }
-    ll sol = 1;
-    for(auto &x: final) {
-        for(int i=0; i<x.second.first; i++) sol *= x.first;
+    //cout << cnt[2] << endl;
+    for(auto &x: factors) {
+        //if(x.first == 2) cout << x.second.second << " " << x.second.first << endl;
+        if(cnt[x.first]<n-1) continue;
+        if(x.second.second == 690 or cnt[x.first] == n-1) sol *= pow(x.first, x.second.first);
+        else sol *= pow(x.first, x.second.second);
     }
+
     cout << sol << endl;
-*/
+}
