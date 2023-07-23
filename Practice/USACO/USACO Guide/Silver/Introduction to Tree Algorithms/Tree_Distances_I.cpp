0c648df157ed0dba0970f7c3f79aeaa578bf8f75
Affe <affeowo@outlook.de>
2023-07-23 13:29:41 +0200

    cses

---
+++ Practice/USACO/USACO Guide/Silver/Introduction to Tree Algorithms/Tree_Distances_I.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[200001];
+int firstMax[200001];   // to store first-max length.
+int secondMax[200001];  // to store second-max length.
+int c[200001];          // to store child for path of max length.
+
+// calculate for every node x the maximum
+// length of a path that goes through a child of x
+void dfs(int v, int p) {
+	firstMax[v] = 0;
+	secondMax[v] = 0;
+	for (auto x : adj[v]) {
+		if (x == p) continue;
+		dfs(x, v);
+		if (firstMax[x] + 1 > firstMax[v]) {
+			secondMax[v] = firstMax[v];
+			firstMax[v] = firstMax[x] + 1;
+			c[v] = x;
+		} else if (firstMax[x] + 1 > secondMax[v]) {
+			secondMax[v] = firstMax[x] + 1;
+		}
+	}
+}
+
+// calculate for every node x the
+// maximum length of a path through its parent p
+void dfs2(int v, int p) {
+	for (auto x : adj[v]) {
+		if (x == p) continue;
+		if (c[v] == x) {
+			if (firstMax[x] < secondMax[v] + 1) {
+				secondMax[x] = firstMax[x];
+				firstMax[x] = secondMax[v] + 1;
+				c[x] = v;
+			} else {
+				secondMax[x] = max(secondMax[x], secondMax[v] + 1);
+			}
+		} else {
+			secondMax[x] = firstMax[x];
+			firstMax[x] = firstMax[v] + 1;
+			c[x] = v;
+		}
+		dfs2(x, v);
+	}
+}
+
+int main() {
+	FIO;
+	int n, u, v;
+	cin >> n;
+	for (int i = 0; i < n - 1; i++) {
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	dfs(1, -1);
+	dfs2(1, -1);
+
+	for (int i = 1; i <= n; i++) { cout << firstMax[i] << " "; }
+	return 0;
+}
+
+/*
+cout << dist[0] << " ";
+    for(int i=1; i<n; i++) {
+        // now go through parents and then child dist + amount of parents
+        int curn = i, pcnt = 1, curmax = dist[i], prev = i;
+        while(true) {
+            pcnt++;
+            curn = par[curn];
+            if(curn == -1) break;
+            for(auto &x: edges[curn]) {
+                if(x!=prev and x != par[curn]) curmax = max(curmax, dist[x]+pcnt);
+            }
+            prev = curn;
+        }
+        curmax = max(curmax, pcnt-2);
+        cout << curmax << " ";
+    }
+    cout << endl;
+*/
