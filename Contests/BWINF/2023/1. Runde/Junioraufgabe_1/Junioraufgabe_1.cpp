ad519d6fab48fb76317a9142d7f23fcda38eff00
Affe <affeowo@outlook.de>
2023-11-15 15:17:30 +0100

    bwinf

--- Contests/BWINF/2023/1. Runde/Junioraufgabe_1/Junioraufgabe_1.cpp
+++ Contests/BWINF/2023/1. Runde/Junioraufgabe_1/Junioraufgabe_1.cpp
@@ -8,19 +8,24 @@ using ld = long double;
 #define all(x) (x).begin(), (x).end()
 #define MOD 1000000007
 
+// main class welche command-line Eingaben annimmt 
 int main(int argc, char* argv[]) {
+    // input von der Datei einlesen, welche in der command-line als Argument gegeben wurde
     ifstream fin(argv[1]);
     
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    // Variablen für Anzahl von Tüten und Spielen definieren und einlesen
     int anzahl_tueten, anzahl_spiele; fin >> anzahl_tueten >> anzahl_spiele;
+    // Anzahl an Spielen pro "Sorte" einlesen und in einem Vector speichern
     vector<int> spiele(anzahl_spiele);
     for(auto &x: spiele) fin >> x;
 
-    // every bag should get the same amount of games in it
+    // Vector welcher für jede Tüte von einem Kind einen Vector speichert in welchem die Spiele gespeichert werden welche in die Tüte kommen
     vector<vector<int>> tueten(anzahl_tueten);
 
+    // Spiele verteilen; Algorithmus in Dokumentation erklärt
     int cur = 0;
     for(int i=0; i<anzahl_spiele; i++) {
         for(int j=0; j<spiele[i]; j++) {
@@ -30,8 +35,9 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // Lösung ausgeben
     for(int i=0; i<anzahl_tueten; i++) {
-        cout << "Tüte " << i+1 << ": ";
+        cout << "T\x81te " << i+1 << ": ";
         for(auto &x: tueten[i]) cout << x << " ";
         cout << endl;
     }
--- Contests/BWINF/2023/1. Runde/Junioraufgabe_2/Junioraufgabe_2.py
+++ Contests/BWINF/2023/1. Runde/Junioraufgabe_2/Junioraufgabe_2.py
@@ -1,19 +1,25 @@
+# Bibliotheken importieren
 from PIL import Image
 from sys import argv
 
+# Bild einlesen
 img = Image.open(argv[1], "r")
+# Breite und Höhe des Bildes
 w, h = img.size
 
+# Daten des Bildes in einer matrix speichern
 mat = list(img.getdata())
 mat = [mat[i:i+w] for i in range(0, len(mat), w)]
 
-sol = chr(mat[0][0][0])
-cur = [mat[0][0][2]%h, mat[0][0][1]%w]
+# sol = Lösungsstring; cur = Koordinaten vom aktuellen Pixel
+sol = ""
+cur = [0, 0]
 
-
+# Buchstaben zum Lösungsstring hinzufügen und zum nächstne Pixel finden bis man wieder am Startpunkt (0, 0) ist
 while True:
     sol += chr(mat[cur[0]][cur[1]][0])
     if mat[cur[0]][cur[1]][1] == 0 and mat[cur[0]][cur[1]][2] == 0: break
     cur = [(cur[0] + mat[cur[0]][cur[1]][2])%h, (cur[1] + mat[cur[0]][cur[1]][1])%w]
 
+# Lösung ausgeben
 print(sol)
