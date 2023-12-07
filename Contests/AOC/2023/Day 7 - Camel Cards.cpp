f3b01b887e5a8dbb469183443176a6a2cf6fa4bc
Affe <affeowo@outlook.de>
2023-12-08 00:18:40 +0100

    aoc

--- Contests/AOC/2023/Day 7 - Camel Cards.cpp
+++ Contests/AOC/2023/Day 7 - Camel Cards.cpp
@@ -8,6 +8,45 @@ using ld = long double;
 #define all(x) (x).begin(), (x).end()
 #define MOD 1000000007
 
+map<char, int> strengths = {{'A', 14}, {'K', 13}, {'Q', 12}, {'J', 11}, {'T', 10}};
+
+int handtype(string hand) {
+    map<char, int> cnt;
+    vector<char> chars;
+    for(auto &x: hand) {
+        if(!cnt.count(x)) chars.push_back(x);
+        cnt[x]++;
+    }
+    if(cnt.size()==1) return 6;
+    else if(cnt.size()==2) {
+        if(cnt[chars[0]]==4 or cnt[chars[0]]==1) return 5;
+        else if(cnt[chars[0]]==3 or cnt[chars[0]]==2) return 4;
+    }
+    else if(cnt.size()==3) {
+        if(cnt[chars[0]]==3 or cnt[chars[1]]==3 or cnt[chars[2]]==3) return 3;
+        else if(cnt[chars[0]]==1 or cnt[chars[1]]==1 or cnt[chars[2]]==1) return 2;
+    }
+    else if(cnt.size()==4) return 1;
+    else return 0;
+    return -1; // idk why but this needs to be here for compiling
+}
+
+bool cstm(pair<string, ll> p1, pair<string, ll> p2) {
+    pair<int, int> types = make_pair(handtype(p1.first), handtype(p2.first)); // 0 = high; 1 = pair; 2 = 2 pairs; 3 = triple; 4 = full house; 5 = 4 of a kind; 6 = 5 of a kind
+
+    if(types.first == types.second) {
+        for(int i=0; i<5; i++) {
+            int val1, val2;
+            if(isdigit(p1.first[i])) val1 = p1.first[i]-'0';
+            else val1 = strengths[p1.first[i]];
+            if(isdigit(p2.first[i])) val2 = p2.first[i]-'0';
+            else val2 = strengths[p2.first[i]];
+            if(val1!=val2) return val1<val2;
+        }
+    }
+    return types.first<types.second;
+}
+
 int main() {
     ifstream fin("input.txt");
     ofstream fout("output.txt");
@@ -15,5 +54,16 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    
+    vector<pair<string, ll>> hands(1000);
+    for(int i=0; i<1000; i++) {
+        string s; getline(fin, s);
+        hands[i].first = s.substr(0, 5);
+        hands[i].second = stoll(s.substr(6, s.size()-5));
+    }
+    sort(all(hands), cstm);
+    for(int i=700; i<710; i++) cout << hands[i].first << endl;
+
+    ll sol1 = 0;
+    for(ll i=0; i<1000; i++) sol1 += hands[i].second*(i+1);
+    fout << sol1 << endl;
 }
