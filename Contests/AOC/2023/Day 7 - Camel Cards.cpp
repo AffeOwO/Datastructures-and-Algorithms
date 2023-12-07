afacb395047c559cbdda12f9007854bee14f9ae5
Affe <affeowo@outlook.de>
2023-12-07 23:35:30 +0100

    aoc

---
+++ Contests/AOC/2023/Day 7 - Camel Cards.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+
+int main() {
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    
+}
