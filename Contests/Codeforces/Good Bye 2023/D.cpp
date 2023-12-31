3549d3283e43fd749193720eb0bce1f2aa82b67e
Affe <affeowo@outlook.de>
2023-12-31 17:39:17 +0100

    cf

---
+++ Contests/Codeforces/Good Bye 2023/D.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+
+void solve() {
+    int n; cin >> n;
+    // output n squares of length n, where the multiset of digits is the same for all
+    // n is odd
+    // x=even-> x² =0;4 (mod 8), x=odd-> x² = 1 (mod8)
+    // last digit in {0, 1, 4, 5, 6, 9}
+    
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while(t--) solve();   
+}
