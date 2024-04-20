eb198cfa408fd6ee79ca71fcb15c963c62eeec1c
Affe <affeowo@outlook.de>
2024-04-20 14:41:49 +0200

    template

--- Practice/template.cpp
+++ Practice/template.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 
+//ifstream fin("");
+//ofstream fout("");
+
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
@@ -9,9 +12,6 @@ using ld = long double;
 #define MOD 1000000007
 
 int main() {
-    //ifstream fin("");
-    //ofstream fout("");
-
     ios::sync_with_stdio(0);
     cin.tie(0);
     
