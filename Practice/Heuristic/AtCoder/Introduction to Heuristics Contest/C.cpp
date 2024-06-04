c6377d7779b4c445fd7b75fb7a9599d941b3a895
Affe <affeowo@outlook.de>
2024-06-04 14:42:13 +0200

    ahc

---
+++ Practice/Heuristic/AtCoder/Introduction to Heuristics Contest/C.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+/* const ll M = (1<<61)-1;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+const ll B = uniform_int_distribution<ll>(0, M - 1)(rng); */
+
+int d; 
+vector<int> c(26), state(d, 0), sol(d, 0);
+vector<vector<int>> mat;
+
+int calc(vector<int> s) {
+    vector<int> last(26, -1);
+    int ret = 0;
+    for(int i=0; i<d; i++) {
+        last[s[i]] = i;
+        ret += mat[i][s[i]];
+        for(int j=0; j<26; j++) ret -= c[j]*(i-last[j]);
+    }
+    return ret;
+}
+
+void solve() {
+    cin >> d;
+    for(auto &x: c) cin >> x;
+    mat.resize(d, vector<int>(26));
+    for(auto &x: mat) {
+        for(auto &y: x) cin >> y;
+    }
+    vector<int> tmp(d);
+    for(auto &x: tmp) {
+        cin >> x;
+        x--;
+    }
+    int m; cin >> m;
+    ll sol = calc(tmp);
+    while(m--) {
+        int i, q; cin >> i >> q;
+        // tmp[i-1] = q-1
+
+        cout << sol << endl;
+    }
+}
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
