015b61d0ef56bc86e9cf1d413ab8c9b6db98a391
Yamato <affeowo@outlook.de>
2024-07-12 19:08:08 +0200

    x

---
+++ Practice/USACO/USACO Guide/Bronze/Additional Practice for USACO Bronze/ABBC_or_BACB.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+/* const ll M = (1<<61)-1;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+const ll B = uniform_int_distribution<ll>(0, M - 1)(rng); */
+
+int main() {
+    //ifstream fin("");
+    //ofstream fout("");
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+}
